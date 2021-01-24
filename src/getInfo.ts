16a53e51748b5aa2e8bfb6230ac7b1f08798877b
JangChorong <sweatpotato13@gmail.com>
2021-01-24 10:55:39 +0900

    feat: Add topcoder command

--- src/getInfo.ts
+++ src/getInfo.ts
@@ -9,11 +9,20 @@ export async function getCodeforcesInfo(handle) {
     return obj;
 }
 
-export async function getAtCoderInfo(handle): Promise<string> {
+export async function getAtCoderInfo(handle) {
     // https://github.com/miozune/AtCoderUsersAPI
     const response = await fetch(
         "https://us-central1-atcoderusersapi.cloudfunctions.net/api/info/username/" +
             handle
+    );
+    const obj = await response.json();
+    return obj;
+}
+
+export async function getTopcoderInfo(handle) {
+    // Use Topcoder official API
+    const response = await fetch(
+        "http://api.topcoder.com/v2/users/" + handle
     );
     const obj = await response.json();
     return obj;
